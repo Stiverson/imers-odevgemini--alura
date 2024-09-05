e9339c2139ae285e33264b09474bc40f9611df2a
Stiverson Rodrigues Machado <stiverson.machado@gmail.com>
2024-09-05 13:03:49 -0300

    atualização no arquivo javascript
    Adição das funções iniciais necessitando adicionar a função de busca.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log(dados);
+// console.log(dados);
+function pesquisar(){}
+
+let section = document.getElementById("resultados-pesquisa")
+
+let resultados = ""
+
+for (let personagen of personagens) {
+
+  resultados += `
+<div class="item-resultado">
+                <h2>
+                    <a href="#">${personagen.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${personagen.descricao}</p>
+                <a href=${personagen.link} target="_blank">Mais informações</a>
+            </div>
+            ` 
+}
+
+section.innerHTML = resultados
+
