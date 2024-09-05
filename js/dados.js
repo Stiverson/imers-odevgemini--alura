889c62c08b76e8cbc6a2093216e2585edb904fba
Stiverson Machado <stiverson.machado@gmail.com>
2024-09-04 23:58:02 -0300

    Aula 3 entendimento javascript criação da base de dados inicial dos personagens com o listas.

---
+++ js/dados.js
@@ -0,0 +1,30 @@
+let  personagens = [
+    {
+        titulo: "Goku",
+        descricao: "Son Goku é um dos personagens de anime mais icônicos e queridos do mundo, protagonista da série Dragon Ball. Criado por Akira Toriyama, Goku é um Saiyajin enviado à Terra quando criança, onde é encontrado e criado pelo avô Gohan. Com um coração puro e uma paixão insaciável por lutas, Goku passa por diversas aventuras, tornando-se mais forte a cada desafio e protegendo a Terra de ameaças cósmicas. Sua jornada é marcada por amizades profundas, transformações épicas e batalhas lendárias, fazendo dele um símbolo de esperança e perseverança para milhões de fãs ao redor do globo.",
+        link: "https://pt.wikipedia.org/wiki/Goku"
+    },
+    {
+        titulo: "Asta",
+        descricao:"Asta é o protagonista do anime Black Clover. Nascido sem nenhum poder mágico em um mundo onde a magia é tudo, Asta se esforça para se tornar o Rei Mago. Com seu grimório anti-magia e um espírito indomável, ele supera obstáculos e se torna um dos magos mais poderosos do reino Clover.",
+        link: "https://pt.wikipedia.org/wiki/Asta_(Black_Clover)"
+    },
+    {
+        titulo: "Ichigo Kurosaki",
+        descricao:"Ichigo Kurosaki é o protagonista do anime Bleach. Um estudante do ensino médio com a habilidade de ver espíritos, Ichigo se torna um Shinigami substituto após um encontro com Rukia Kuchiki. Com seu poderoso Zanpakuto, Zangetsu, ele protege o mundo humano dos Hollows e explora a Soul Society.",
+        link: "https://pt.wikipedia.org/wiki/Ichigo_Kurosaki"
+    },
+      {
+        titulo: "Monkey D. Luffy",
+        descricao: "Monkey D. Luffy é o protagonista do anime One Piece. Um jovem pirata que sonha em se tornar o Rei dos Piratas, Luffy possui a habilidade de esticar seu corpo à vontade graças a uma fruta do diabo. Com sua tripulação, os Piratas do Chapéu de Palha, ele navega pelo Grand Line em busca do tesouro One Piece.",
+        link: "https://pt.wikipedia.org/wiki/Monkey_D._Luffy"
+      },
+      {
+        titulo: "Light Yagami",
+        descricao: "Light Yagami é o protagonista do anime Death Note. Um estudante brilhante que encontra um caderno sobrenatural com o poder de matar qualquer pessoa cujo nome seja escrito nele. Light embarca em uma cruzada para criar um mundo livre do crime, assumindo a identidade de Kira.",
+        link: "https://pt.wikipedia.org/wiki/Light_Yagami"
+      }
+    
+]
+
+
