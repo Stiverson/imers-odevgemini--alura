282d7e4c0231f3f8f6a338a683b0d8709052c39e
Stiverson Rodrigues Machado <stiverson.machado@gmail.com>
2024-09-06 12:59:29 -0300

    atualização aula 5
    atualização para o botão do campo de pesquisas dos personagens

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 // console.log(dados);
 function pesquisar(){
     let section = document.getElementById("resultados-pesquisa")
+    
+let campoPesquisa = document.getElementById ("campo-pesquisa").value
 
+    if (campoPesquisa == ""){
+        section.InnerHtml = "<p>Esse personagen ainda não esta cadastrado na nossa base de dados<p>"
+        return
+    }
+    
+    
 let resultados = ""
 
 for (let personagen of personagens) {
 
-  resultados += `
+   if (personagen.titulo.includes(campoPesquisa)){
+         resultados += `
 <div class="item-resultado">
                 <h2>
                     <a href="#">${personagen.titulo}</a>
@@ -14,7 +23,9 @@ for (let personagen of personagens) {
                 <p class="descricao-meta">${personagen.descricao}</p>
                 <a href=${personagen.link} target="_blank">Mais informações</a>
             </div>
-            ` 
+            ` ;
+   }
+
 }
 
 section.innerHTML = resultados
